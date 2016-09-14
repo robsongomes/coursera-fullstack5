3ada6d947166bee6f4b534cde7f33598268192dd
Robson Gomes <Robson Gomes>
2016-09-13 23:31:38 -0300

    Some fixes

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -2,12 +2,15 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope']
+LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope) {
 
   $scope.check = function() {
     //I'm considering empty as an item
+    if ($scope.dishes === '') {
+      $scope.message = 'Please enter data first';
+    }
     if ($scope.dishes.split(',').length <= 3) {
       $scope.message = "Enjoy!";
     } else {
